5815295bc22dc89a4553ddba2b7a6a43721936fc
Fredrick Mweu <freddyjames534@gmail.com>
2023-07-26 16:28:17 -0700

    comments out

--- getinfo.c
+++ getinfo.c
@@ -1,9 +1,5 @@
 #include "shell.h"
 
-/**
- * clear_info - initializes info_t struct
- * @info: struct address
- */
 void clear_info(info_t *info)
 {
 	info->arg = NULL;
@@ -12,11 +8,6 @@ void clear_info(info_t *info)
 	info->argc = 0;
 }
 
-/**
- * set_info - initializes info_t struct
- * @info: struct address
- * @av: argument vector
- */
 void set_info(info_t *info, char **av)
 {
 	int i = 0;
@@ -43,11 +34,6 @@ void set_info(info_t *info, char **av)
 	}
 }
 
-/**
- * free_info - frees info_t struct fields
- * @info: struct address
- * @all: true if freeing all fields
- */
 void free_info(info_t *info, int all)
 {
 	ffree(info->argv);
@@ -64,7 +50,7 @@ void free_info(info_t *info, int all)
 		if (info->alias)
 			free_list(&(info->alias));
 		ffree(info->environ);
-			info->environ = NULL;
+		info->environ = NULL;
 		bfree((void **)info->cmd_buf);
 		if (info->readfd > 2)
 			close(info->readfd);
