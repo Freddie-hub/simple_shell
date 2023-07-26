431b26cd3f7a0cbc2361d85cc183187e62329fbd
Fredrick Mweu <freddyjames534@gmail.com>
2023-07-26 15:26:19 -0700

    restored

--- shell.h
+++ shell.h
@@ -1,17 +1,16 @@
-#define _SHELL_H_
 #ifndef _SHELL_H_
+#define _SHELL_H_
 
-#include <stdlib.h>
 #include <stdio.h>
-
-#include <limits.h>
-#include <fcntl.h>
-#include <errno.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
 
 /* for read/write buffers */
 #define READ_BUF_SIZE 1024
